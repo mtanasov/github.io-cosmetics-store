4b885c9a70c9d128c9b99ca35fe8eab1a0ff9981
Mykhailo <mtanasov@gmail.com>
2023-03-21 11:16:19 +0200

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function Header() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="">
+
+      <img src="https://static.insales-cdn.com/files/1/3337/22768905/original/newlogo.png" alt="Moon" title="Moon"></img>
+    </div>
+  )
+}
+
+export default Header
