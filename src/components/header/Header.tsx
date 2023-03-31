5d1d20baa3b2f81a5106537f79f6fe2140dd0eb1
Mykhailo <mtanasov@gmail.com>
2023-03-31 13:20:55 +0300

    header effect hover work

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,47 +1,43 @@
-import { useState, useEffect } from 'react'
-import "./style__header.css"
-
+import { useState, useEffect } from "react";
+import "./style__header.css";
 
 export default () => {
-
   const [trigger, setTrigger] = useState(false);
   const [bgrColor, setBgrColor] = useState("transparent");
 
-  const bgr = () => { // для эффекта наведения
-    setTrigger(!trigger)
+  const bgr = () => {
+    // для эффекта наведения
+    setTrigger(!trigger);
     console.log(trigger);
-  }
+  };
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      window.scrollY > 20 ? setTrigger(true) : setTrigger(false)
-    })
-  })
-
+      window.scrollY > 20 ? setTrigger(true) : setTrigger(false);
+    });
+  });
 
   return (
     <header
-      className='header'
-      onMouseOver={bgr}
-      onMouseOut={bgr}
-      style={
-        {
-          backgroundColor: trigger ? "rgb(255, 255, 255)" : "transparent"
-        }
-      }
+      className="header"
+      onMouseEnter={bgr}
+      onMouseLeave={bgr}
+      style={{
+        backgroundColor: trigger ? "rgb(255, 255, 255)" : "transparent",
+      }}
     >
-      <div className='nav'>
-        <button className='menu__catalogue'>Каталог</button>
-        <button className='menu__top' >Верхнее меню</button>
-        <button className='menu__bottom' >Нижнее меню</button>
+      <div className="nav">
+        <button className="menu__catalogue">Каталог</button>
+        <button className="menu__top">Верхнее меню</button>
+        <button className="menu__bottom">Нижнее меню</button>
       </div>
 
-      <div className="logo">
+      <div className="logo"></div>
+      <div className="controls">
+        <input className="search" type="text" placeholder="Поиск" />
+        <button className="user icon-btn_header"></button>
+        <button className="basket icon-btn_header"></button>
       </div>
-      <div className='controls' >
-        <input className='search' type="text" placeholder='Поиск' />
-        <button className='basket icon-btn_header' ></button>
-      </div>
-    </header >
-  )
-}
+    </header>
+  );
+};
